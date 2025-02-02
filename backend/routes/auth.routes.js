f97e26c3ee2d56dd9e68b751446db6a7d8002fb7
shayan <syedshayan1324@gmail.com>
2025-02-02 14:47:09 +0500

    vercel.json updated

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,14 +1,8 @@
 const express = require("express")
-const { checkAuth } = require("../controller/auth.controller")
-const { logOut } = require("../controller/auth.controller")
+const { checkAuth, logOut } = require("../controller/auth.controller")
 const router = express.Router()
 
-<<<<<<< HEAD
 router.post("/", checkAuth);
 router.get("/logout" ,logOut);
-=======
-router.post("/", checkAuth)
-router.get("/logout" ,logOut)
->>>>>>> 7f07386e89aa5d8ef57121ed9be6e2e4233ff4dc
 
 module.exports = router
